4500dbed56688afb5d3954d9942c323061947594
Saurabh Singh <offical.saurabhsingh@gmail.com>
2025-11-05 21:26:02 +0530

    Update CORS origin to production URL

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ import userRouter from "./routes/user.routes.js"
 
 const app=express()
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://ai-virtual-assistant-12.onrender.com",
     credentials:true
 }))
 const port=process.env.PORT || 5000

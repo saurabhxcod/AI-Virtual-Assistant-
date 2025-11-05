42a5de9b71f8b5f2da9f9c70090e9dc42c7207f5
saurabhxcod <offical.saurabhsingh@gmail.com>
2025-11-05 19:16:37 +0530

    modified database

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 const connectDb=async ()=>{
     try {
         await mongoose.connect(process.env.MONGODB_URL)
-        console.log("MongoDB Connected")
+        console.log("MongoDB Connected!!")
     } catch (error) {
         console.log(error)
     }
